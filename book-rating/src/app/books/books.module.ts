a033ceaa3950625b86c27b9690fe84f4421023b7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-17 18:39:24 +0200

    empty CreateBookComponent

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { BooksRoutingModule } from './books-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { BooksRoutingModule } from './books-routing.module';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToArrayPipe } from './shared/to-array.pipe';
-
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    ToArrayPipe
+    ToArrayPipe,
+    CreateBookComponent
   ],
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ReactiveFormsModule
   ],
   exports: [
     DashboardComponent
