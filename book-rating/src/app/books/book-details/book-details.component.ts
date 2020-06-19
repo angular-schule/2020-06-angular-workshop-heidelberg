222e2a2fb02a1c07f5116afe32dcb9d6540c92a8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-19 11:46:52 +0200

    rxjs: flatten --> mergeMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, timer, Subscription, Observable } from 'rxjs';
-import { take, map, filter, reduce, repeat, concatMap } from 'rxjs/operators';
+import { take, map, filter, reduce, repeat, concatMap, mergeMap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 
@@ -24,11 +24,9 @@ export class BookDetailsComponent {
 
     this.route.paramMap.pipe(
       map(paramMap => paramMap.get('isbn')),
-      map(isbn => this.bs.getSingleBook(isbn))
+      mergeMap(isbn => this.bs.getSingleBook(isbn))
     )
-    .subscribe(book$ => book$.subscribe(
-      book => console.log(book))
-    );
+    .subscribe(book => console.log(book));
   }
 
 
