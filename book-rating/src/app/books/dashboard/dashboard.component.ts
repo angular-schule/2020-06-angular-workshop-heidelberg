3f0fbe24cea0bfc9d75a3c632becea259e3aca83
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-19 16:54:38 +0200

    ngrx: final example

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -2,47 +2,56 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { Store, select } from '@ngrx/store';
+import { loadBooks } from '../store/book.actions';
+import { selectBooks, selectBooksLoading, selectBookViaIsbn } from '../store/book.selectors';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  // VORSICHT
-  // erzeugt einen Bug, sobald wir AJAX machen!!
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
 
-  books: Book[] = [];
+  books$ = this.store.pipe(select(selectBooks));
+  loading$ = this.store.pipe(select(selectBooksLoading));
 
-  constructor(private br: BookRatingService, private bs: BookStoreService) {
+  specialBook$ = this.store.pipe(select(selectBookViaIsbn, { isbn: '42' }));
+
+  constructor(private store: Store) {
+    this.store.dispatch(loadBooks());
+
   }
 
   ngOnInit(): void {
-    this.bs.getBooks().subscribe(books => this.books = books);
+  }
+
+  changeSpecialBook(isbn: string) {
+    this.specialBook$ = this.store.pipe(select(selectBookViaIsbn, { isbn }));
   }
 
   doRateDown(book: Book) {
-    const ratedBook = this.br.rateDown(book);
-    this.update(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.update(ratedBook);
   }
 
   doRateUp(book: Book) {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: book.rating < 5 ?  ++book.rating : 5
-    // };
-    this.update(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: book.rating < 5 ?  ++book.rating : 5
+    // // };
+    // this.update(ratedBook);
   }
 
   update(ratedBook: Book) {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating);
   }
 
   addBook(newBook: Book) {
-    this.books = [...this.books, newBook];
+    // this.books = [...this.books, newBook];
   }
 }
--- book-rating/src/app/books/store/book.selectors.ts
+++ book-rating/src/app/books/store/book.selectors.ts
@@ -4,3 +4,21 @@ import * as fromBook from './book.reducer';
 export const selectBookState = createFeatureSelector<fromBook.State>(
   fromBook.bookFeatureKey
 );
+
+export const selectBooksLoading = createSelector(
+  selectBookState,
+  state => state.loading
+);
+
+export const selectBooks = createSelector(
+  selectBookState,
+  state => state.books
+);
+
+
+// computed selector
+
+export const selectBookViaIsbn = createSelector(
+  selectBooks,
+  (state, props) => state.find(b => b.isbn === props.isbn)
+);
