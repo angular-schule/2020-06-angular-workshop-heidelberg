d40508c12f390fbf973868cf6168fec4e46c4f60
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-19 14:13:14 +0200

    rxjs: example forkJoin

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, timer, Subscription, Observable } from 'rxjs';
-import { take, map, filter, reduce, repeat, concatMap, mergeMap, switchMap, share, shareReplay, catchError, retry } from 'rxjs/operators';
+import { of, timer, Subscription, Observable, forkJoin } from 'rxjs';
+import { take, map, filter, reduce, repeat, concatMap, mergeMap, switchMap, share, shareReplay, catchError, retry, tap } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -17,14 +17,21 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn).pipe(
-      retry(3),
-      catchError((err: HttpErrorResponse) => of({
-        isbn: '???',
-        title: 'Fehler',
-        description: err.message
-      }))
-    ))
+    switchMap(isbn => forkJoin(
+      [
+        this.bs.getSingleBook(isbn).pipe(
+          retry(3),
+          catchError((err: HttpErrorResponse) => of({
+            isbn: '???',
+            title: 'Fehler',
+            description: err.message
+          }))
+        ),
+        this.bs.getBooks()
+      ])),
+    tap(([book, books]) => console.log(book, books)),
+    // tap(arr => console.log(arr[0], arr[1])),
+    map(([book]) => book)
   );
 
   constructor(
