f0082354ea09b712d6c8060df2a1d25c59982701
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-19 12:18:24 +0200

    rxjs: change

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { of, timer, Subscription, Observable } from 'rxjs';
-import { take, map, filter, reduce, repeat, concatMap, mergeMap, switchMap } from 'rxjs/operators';
+import { take, map, filter, reduce, repeat, concatMap, mergeMap, switchMap, share } from 'rxjs/operators';
 import { BookStoreService } from '../shared/book-store.service';
 
 
@@ -14,7 +14,8 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    switchMap(isbn => this.bs.getSingleBook(isbn))
+    switchMap(isbn => this.bs.getSingleBook(isbn)),
+    share()
   );
 
   constructor(
