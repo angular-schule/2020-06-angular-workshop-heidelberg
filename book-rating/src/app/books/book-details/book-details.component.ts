2c5714dcbe6770e4c62b282eb6fd7e2e7a8df483
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2020-06-19 11:25:09 +0200

    -- RXJS: TABULA RASA ---

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -9,43 +9,16 @@ import { take, map, filter, reduce, repeat, concatMap } from 'rxjs/operators';
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit, OnDestroy {
+export class BookDetailsComponent implements OnInit {
 
   isbn: string;
-  sub: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap
       .subscribe(paramMap => this.isbn = paramMap.get('isbn'));
-
-    // 1. Baustein: Observer
-    const observer = {
-      next: s => console.log(s),
-      error: err => console.log('FEHLER', err),
-      complete: () => console.log('COMPLETE!')
-    };
-
-    // 2. Baustein: Observable
-    const observable = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-    observable.pipe(
-      map(x => x * 10),
-      filter(x => x >= 30),
-      reduce((a, b) => a + b),
-      concatMap(x => of('😎')
-        .pipe(repeat(x))
-      )
-    ).subscribe(observer);
-
-
-
-
   }
 
-  ngOnDestroy() {
-    // this.sub.unsubscribe();
-  }
 
 }
